849c7f7496d069d96679f4777de5e521cda7e355
Adriana <adrianam754@gmail.com>
2022-07-02 18:13:38 -0300

    fix: Change the delimiter in read

--- src/jobs.py
+++ src/jobs.py
@@ -7,7 +7,7 @@ from functools import lru_cache
 def read(path):
 
     with open(path, encoding="utf-8") as csv_file:
-        reader_dict = csv.DictReader(csv_file, delimiter=";", quotechar='"')
+        reader_dict = csv.DictReader(csv_file, delimiter=",", quotechar='"')
 
         jobs_list = list(reader_dict)
 
