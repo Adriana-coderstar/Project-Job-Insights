cfac1c8d6f39b5874ae1d04e785d7ea8c8807ad4
Adriana <adrianam754@gmail.com>
2022-07-05 11:13:09 -0300

    test: add test count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    total_python_words = count_ocurrences("src/jobs.csv", "python")
+    assert total_python_words == 1639
+
+    total_javascript_words = count_ocurrences("src/jobs.csv", "javascript")
+    assert total_javascript_words == 122
